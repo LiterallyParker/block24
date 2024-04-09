a81264ccb8654616f995b327d611090e45f61efd
LiterallyParker <parkernash2001@gmail.com>
2024-04-09 04:02:10 -0500

    final product

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,40 @@ import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  const [puppy, setPuppy] = useState(null)
 
-  console.log(puppies)
+  function handleClick() {
+    console.log('Hello')
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup)
 
   return (
-    <div className='App'>
+    <div className='App' key="App">
+      <h1>Check out these dogs!</h1>
+      <div className='UI'>
+
+      {featPupId && 
+      <div className='featPup' key='featPup'>
+            <h2>{featuredPup.name}</h2>
+            <h3>Age: {featuredPup.age}</h3>
+            <h3>Email: {featuredPup.email}</h3>
+          </div>}
+      <div>
+
     {puppies.map((puppy) => {
       return (
-          <p key={puppy.id}>{puppy.name}</p>
-        )
-      }
-    )}
-    </div>
+        <div className="pup" key={`pup-div-${puppy.id}`}>
+        <p onClick={() => {setFeatPupId(puppy.id)} } key={puppy.id}>{puppy.name}</p>
+        </div>
+      )
+    }
+  )}
+  </div>
+  </div>
+  </div>
   )
 }
 
