c928d963c0c2ed5b84214f2bfd2998fabcb7831a
LiterallyParker <parkernash2001@gmail.com>
2024-04-09 03:20:17 -0500

    commit 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { puppyList } from './data'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+
+  console.log(puppies)
 
   return (
-    <>
-    </>
+    <div className='App'>
+    {puppies.map((puppy) => {
+      return (
+          <p key={puppy.id}>{puppy.name}</p>
+        )
+      }
+    )}
+    </div>
   )
 }
 
